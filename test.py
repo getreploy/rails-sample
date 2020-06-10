5c8cdf6b6ae643ced1a83d174f751fc0d4835c4c
Jay Khatri <khatri.florida@gmail.com>
2020-06-10 14:48:02 -0400

    change

--- test.py
+++ test.py
@@ -1,11 +1,21 @@
 import subprocess
+import os
 
-cmd = ['git', 'clone', 'https://fc723a84133d367841bec6eef2b74df3c7b2843b@github.com/getreploy/rails-sample.git', '270921786']
-try:
-    out = subprocess.check_output(cmd)
-except Exception as e:
-    print("error cloning directory")
-    print(e.output.decode())
-    exit(0)
+is_dir = False
+if os.path.isdir("270921786"):
+    is_dir = True
+
+print(is_dir)
+
+if not is_dir:
+    cmd = ['git', 'clone', 'https://fc723a84133d367841bec6eef2b74df3c7b2843b@github.com/getreploy/rails-sample.git', '270921786']
+    try:
+        out = subprocess.check_output(cmd)
+    except Exception as e:
+        print("error cloning directory")
+        print(e.output.decode())
+        exit(0)
 
 
+output = subprocess.check_output('ls -a && pwd && cd 270921786 && git checkout 4238067', shell=True)
+print(output)
